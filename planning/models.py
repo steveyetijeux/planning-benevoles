7013e71b36376c3117ff0320dfc3114ce71352f4
Ethan Puget <steveyetijeux@gmail.com>
2026-05-09 16:21:11 +0200

    replace email with telephone

--- planning/models.py
+++ planning/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Benevole(models.Model):
     nom = models.CharField(max_length=100)
-    email = models.EmailField()
+    telephone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nom

47e37f7c50a8ab3d6aedbb5a333f30ee0d5eb95f
Ethan Puget <steveyetijeux@gmail.com>
2026-07-05 23:03:31 +0200

    fix middleware function version safe

--- config/middleware.py
+++ config/middleware.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.conf import settings
 
+def MaintenanceModeMiddleware(get_response):
 
-class MaintenanceModeMiddleware:
-    def _init_(self, get_response):
-        self.get_response = get_response
-
-    def _call_(self, request):
+    def middleware(request):
 
         if getattr(settings, "MAINTENANCE_MODE", False):
 
             if request.path.startswith("/admin"):
-                return self.get_response(request)
+                return get_response(request)
 
             return render(request, "maintenance.html", status=503)
 
-        return self.get_response(request)
+        return get_response(request)
+
+    return middleware
+
+print("MIDDLEWARE CHARGE")

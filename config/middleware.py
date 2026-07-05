21f17572e1ab34c695fd18b6569ebfd7dc3253bf
Ethan Puget <steveyetijeux@gmail.com>
2026-07-05 22:40:45 +0200

    fix middleware init django

--- config/middleware.py
+++ config/middleware.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
 
+
 class MaintenanceModeMiddleware:
     def _init_(self, get_response):
         self.get_response = get_response
 
     def _call_(self, request):
 
-        # Autoriser l'admin même en maintenance
+        # Autoriser admin même en maintenance
         if request.path.startswith("/admin"):
             return self.get_response(request)
 
-        # Si mode maintenance activé
+        # Mode maintenance activé
         if getattr(settings, "MAINTENANCE_MODE", False):
             return render(request, "maintenance.html", status=503)
 

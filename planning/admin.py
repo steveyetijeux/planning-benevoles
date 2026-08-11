5c3aa5e7b51a706455a988ca0b8cb4cd6c28e8ee
Ethan Puget <steveyetijeux@gmail.com>
2026-08-11 22:41:46 +0200

    système de thème valide avant - nouvelle entrée heure créneau

--- planning/admin.py
+++ planning/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
+from django import forms
 from .models import Benevole, Creneau, Inscription, SiteSettings
 
 
+class CreneauAdminForm(forms.ModelForm):
+    class Meta:
+        model = Creneau
+        fields = '__all__'
+        widgets = {
+            'heure_debut': forms.TimeInput(
+                format='%H:%M',
+                attrs={
+                    'type': 'text',
+                    'placeholder': 'HH:MM',
+                }
+            ),
+            'heure_fin': forms.TimeInput(
+                format='%H:%M',
+                attrs={
+                    'type': 'text',
+                    'placeholder': 'HH:MM',
+                }
+            ),
+        }
+
+
+@admin.register(Creneau)
+class CreneauAdmin(admin.ModelAdmin):
+    form = CreneauAdminForm
+
+
 admin.site.register(Benevole)
-admin.site.register(Creneau)
 admin.site.register(Inscription)
 
 
